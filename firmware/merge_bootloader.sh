1325ac75eda2a30a8daab9cd9d9ca6e9738d44ba
The Things Products <info@thethingsproducts.com>
2018-02-23 18:43:59 +0100

    Dynamic path per platform

--- firmware/merge_bootloader.sh
+++ firmware/merge_bootloader.sh
@@ -2,4 +2,13 @@
 
 set -e
 
-/opt/microchip/mplabx/v3.45/mplab_ide/platform/../mplab_ide/modules/../../bin/hexmate --edf=/opt/microchip/mplabx/v3.45/mplab_ide/platform/../mplab_ide/modules/../../dat/en_msgs.txt TTN_Gateway.X/dist/TTN_Gateway_v1/production/TTN_Gateway.X.production.hex bootloader.hex -ofirmware-with-bootloader.hex
+if [ "$(uname -s)" = "Darwin" ]; then
+  export PATH="$PATH:/Applications/microchip/xc32/v1.42/bin:/Applications/microchip/mplabx/v3.45/mplab_ide/bin"
+elif [ "$(uname -s)" = "Linux" ]; then
+  export PATH="$PATH:/opt/microchip/xc32/v1.42/bin:/opt/microchip/mplabx/v3.45/mplab_ide/bin"
+else
+  echo "We're not sure how to handle your platform or OS. Please add your platform or OS to compile.sh and submit a pull request."
+  exit 1
+fi
+
+hexmate TTN_Gateway.X/dist/TTN_Gateway_v1/production/TTN_Gateway.X.production.hex bootloader.hex -ofirmware-with-bootloader.hex
