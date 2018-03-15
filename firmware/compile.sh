b9c115e3f0104d0b0db025599a2ccc346e7591fd
The Things Products <info@thethingsproducts.com>
2018-03-15 10:27:22 +0100

    Added compiler options to speed up compilation while debugging

--- firmware/compile.sh
+++ firmware/compile.sh
@@ -3,6 +3,26 @@
 set -e
 set -o pipefail
 
+GENERATE_PROJECT_MAKEFILES=true
+GENERATE_DISASSEMBLY=true
+
+for i in "$@"
+do
+case $i in
+    --skip-project)
+    GENERATE_PROJECT_MAKEFILES=false
+    shift
+    ;;
+    --skip-disassembly)
+    GENERATE_DISASSEMBLY=false
+    shift
+    ;;
+    *)
+          # unknown option
+    ;;
+esac
+done
+
 if [ "$(uname -s)" = "Darwin" ]; then
   export PATH="$PATH:/Applications/microchip/xc32/v1.42/bin"
   prjMakefilesGenerator='/Applications/microchip/mplabx/v3.45/mplab_ide.app/Contents/Resources/mplab_ide/bin/prjMakefilesGenerator.sh'
@@ -14,6 +34,20 @@ else
   exit 1
 fi
 
-$prjMakefilesGenerator -v TTN_Gateway.X@TTN_Gateway_v1
+if [ $GENERATE_PROJECT_MAKEFILES = true ]; then
+  echo "Generation of project make files"
+  $prjMakefilesGenerator -v TTN_Gateway.X@TTN_Gateway_v1
+else
+  echo "Skipping generation of project make files"
+fi
+
 make -j -C TTN_Gateway.X/ -f nbproject/Makefile-TTN_Gateway_v1.mk SUBPROJECTS= .build-conf 2>&1 | tee compile_log.txt
+
+if [ $GENERATE_DISASSEMBLY = true ]; then
+  echo "Generation of disassembly"
+  xc32-objdump -S TTN_Gateway.X/dist/TTN_Gateway_v1/production/TTN_Gateway.X.production.elf > disassembly.txt
+else
+  echo "Skipping generation of disassembly"
+fi
+
 
