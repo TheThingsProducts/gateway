de41c83356a28294bea3f20eea0bad0828d8c0fb
Marten Lootsma <m.lootsma@gmail.com>
2018-07-07 08:49:23 +0200

    Corrected download if Azure signature

--- .ci/install-azure-cli.sh
+++ .ci/install-azure-cli.sh
@@ -2,7 +2,7 @@
 
 set -e -o pipefail
 
-apt-key adv --keyserver packages.microsoft.com --recv-keys 52E16F86FEE04B979B07E28DB02C46DF417A0893
+curl -L https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $(lsb_release -cs) main" > /etc/apt/sources.list.d/azure-cli.list
 apt-get update
 apt-get install -y azure-cli jq
